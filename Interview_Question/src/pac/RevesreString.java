8b2a66ed6c7dfacdf98daedffb20cb6315641997
Abhishek75-00 <130983558+Abhishek75-00@users.noreply.github.com>
2023-09-08 13:22:13 +0530

    updates

---
+++ Interview_Question/src/pac/RevesreString.java
@@ -0,0 +1,13 @@
+package pac;
+
+public class RevesreString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    String a="BHARATH REDDY";
+    for(int i=a.length()-1;i>=0;i--) {
+    	System.out.print(a.charAt(i));
+    }
+	}
+
+}
